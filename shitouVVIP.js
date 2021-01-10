5978334cd095598f7441c59eca0f37e48c3c713d
1448030937 <74591676+1448030937@users.noreply.github.com>
2021-01-10 23:03:06 +0800

    Create shitouVVIP.js

---
+++ shitouVVIP.js
@@ -0,0 +1,19 @@
+/*
+https://app.qdstt.cn/api/apiClient/index url script-response-body https://raw.githubusercontent.com/1448030937/My2/main/shitouVVIP.js
+*/
+
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/api/apiClient/index';
+
+if (url.indexOf(vip) != -1) {
+	obj.data["vvip"] = "1";
+	obj.data["vvip_deadline"] = "1835312949000";
+	obj.data["vip"] = "1";
+	body = JSON.stringify(obj);
+}
+
+$done({body});
